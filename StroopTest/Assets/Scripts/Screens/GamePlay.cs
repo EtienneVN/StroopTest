3a26084ff9c56317ef792cc0cdaeb35677190fb2
Eddy <young.reaper12@gmail.com>
2021-12-12 00:02:01 +1030

    Gameplay - [TODO] Check for duplicate button colors

--- StroopTest/Assets/Scripts/Screens/GamePlay.cs
+++ StroopTest/Assets/Scripts/Screens/GamePlay.cs
@@ -30,6 +30,10 @@ public class GamePlay : MonoBehaviour
     [Header("COLOURS")]
     public List<String> colours;
 
+    [Space]
+    [Header("Current Colors")]
+    public List<String> buttonColors;
+
     #endregion
 
     #region PROPERTIES
@@ -62,23 +66,71 @@ public class GamePlay : MonoBehaviour
 
     [Button("TestColour")]
     private void testColour() {
+        string randColour = randomColour();
+
+        if ( TestObject.text == randColour )
+            testColour();
+
         TestObject.text = randomColour();
         testText = TestObject.text;
+
     }
 
+    // TODO - Check duplicate colors and change them to a different colour
     [Button("TestButtons")]
     private void testButton() {
+        // Clear the button text
+        buttonColors.Clear();
         foreach ( var button in PlayerButtons ) {
-            button.GetComponentInChildren<Text>().text = randomColour();
+            button.GetComponentInChildren<Text>().text = "";
+        }
+
+        int r = UnityEngine.Random.Range(0, 4);
+        PlayerButtons[r].GetComponentInChildren<Text>().text = testText;
+        buttonColors.Add(testText);
+
+        // set button text
+        foreach ( var button in PlayerButtons ) {
+            String randCol = randomColour(testText);
+            if ( button.GetComponentInChildren<Text>().text == "" ) {
+                buttonColors.Add(randCol);
+                button.GetComponentInChildren<Text>().text = randCol;
+            }
         }
     }
 
+    /// <summary>
+    /// Return a random colour
+    /// </summary>
+    /// <returns></returns>
     String randomColour() {
         int r = UnityEngine.Random.Range(0, colours.Count);
         return colours[r];
     }
+    String randomColour(String s) {
+        int r = UnityEngine.Random.Range(0, colours.Count);
+
+        if ( s != colours[r] ) {
+            return colours[r];
+        }
+
+        return randomColour(s);
+    }
+
+    bool checkDuplicateColours() {
+        foreach ( var buttonText in PlayerButtons ) {
+            foreach ( var buttonColor in buttonColors ) {
+                if ( buttonText.GetComponentInChildren<Text>().text == buttonColor ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
     private void reroll() {
+        selectedColour = null;
+        buttonColors.Clear();
         testColour();
         testButton();
     }

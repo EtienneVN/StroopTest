2f256dac96c9470751d45df8bc81d94079929b1d
Eddy <young.reaper12@gmail.com>
2021-12-24 23:31:33 +1030

    implemented Player properties

--- StroopTest/Assets/Scripts/ManagerScripts/GameManager.cs
+++ StroopTest/Assets/Scripts/ManagerScripts/GameManager.cs
@@ -5,9 +5,12 @@ using System.Runtime.Serialization;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
+/// <summary>
+/// Responsible for the games state and player data
+/// </summary>
 public class GameManager : MonoBehaviour
 {
-    public static GameManager _instance;
+    public static GameManager Instance;
 
     #region CONSTRUCTORS
 
@@ -15,10 +18,13 @@ public class GameManager : MonoBehaviour
 
     #region STRUCTS
 
-    public struct Player
+    /// <summary>
+    /// A data structure containing the data for the player 
+    /// </summary>
+    private struct Player
     {
-        public int health;
-        public int score;
+        public int Health;
+        public int Score;
         public float TotalTime;
     }
 
@@ -26,12 +32,15 @@ public class GameManager : MonoBehaviour
 
     #region ENUMS
 
+    /// <summary>
+    /// A set number of states that the game will transition through
+    /// </summary>
     public enum GameState
     {
-        TITLE = 0,
-        OPTIONS = 1,
-        GAMEPLAY = 2,
-        PAUSE = 3,
+        Title = 0,
+        Options = 1,
+        Gameplay = 2,
+        Pause = 3,
         PostGame = 4
     }
 
@@ -39,7 +48,8 @@ public class GameManager : MonoBehaviour
 
     #region PRIVATE MEMBERS
 
-    private int stateNum;
+    private int _stateNum = 1;
+    private const string TestString = "Test Buttons"; 
 
     #endregion
 
@@ -47,61 +57,64 @@ public class GameManager : MonoBehaviour
 
     [Space]
     [Header("PLAYER OBJECT")]
-    public Player player;
+    [Tooltip("Object for the player")]
+    [SerializeField] private Player player;
 
     [Header("SCREEN OBJECTS")]
-    public GameObject TitleScreen;
-    public GameObject GamePlay;
-    public GameObject Options;
-    public GameObject Pause;
-    public GameObject PostGame;
+    [Tooltip("GameObject for the title screen")]
+    public GameObject titleScreen;
+    [Tooltip("Gameobject shown during the gameplay")]
+    public GameObject gamePlay;
+    [Tooltip("Gameobject for the options menu")]
+    public GameObject options;
+    [Tooltip("Gameobject shown during the pause menu")]
+    public GameObject pause;
+    [Tooltip("Gameobject Shown during the post game menu")]
+    public GameObject postGame;
 
     [Space(10)]
     [Header("GAME STATE")]
-    public GameState currentState = GameState.TITLE;
-    public GameState previousState = GameState.TITLE;
+    [Tooltip("Shows the current game state")]
+    public GameState currentState = GameState.Title;
+    [Tooltip("Shows the previous games state")]
+    public GameState previousState = GameState.Title;
 
     #endregion
 
     #region MONOBEHAVIOUR
 
     private void Awake() {
-        if ( !_instance ) _instance = this;
+        if ( !Instance ) Instance = this;
         DontDestroyOnLoad(this);
         _init();
-    }
-
-    // Start is called before the first frame update
-    void Start() {
-
     }
 
     // Update is called once per frame
     void Update() {
         switch (currentState) {
 
-            case GameState.TITLE:
+            case GameState.Title:
                 ClearScreen();
-                TitleScreen.SetActive(true);
+                titleScreen.SetActive(true);
                 break;
-            case GameState.OPTIONS:
-                if ( previousState != GameState.GAMEPLAY ) { ClearScreen(); }
-                Options.SetActive(true);
+            case GameState.Options:
+                if ( previousState != GameState.Gameplay ) { ClearScreen(); }
+                options.SetActive(true);
                 break;
-            case GameState.GAMEPLAY:
+            case GameState.Gameplay:
                 ClearScreen();
-                GamePlay.SetActive(true);
+                gamePlay.SetActive(true);
                 player.TotalTime += Time.deltaTime;
                 break;
-            case GameState.PAUSE:
-                Pause.SetActive(true);
+            case GameState.Pause:
+                pause.SetActive(true);
                 break;
             case GameState.PostGame:
                 ClearScreen();
-                PostGame.SetActive(true);
+                postGame.SetActive(true);
                 break;
             default:
-                currentState = GameState.TITLE;
+                currentState = GameState.Title;
                 _init();
                 break;
         }
@@ -111,53 +124,80 @@ public class GameManager : MonoBehaviour
 
     #region PUBLIC PROPERTIES
 
-    public int PlayerHealth { get => player.health; set => player.health = value; }
-
+    public int PlayerHealth { get => player.Health; set => player.Health = value; }
+    public int PlayerScore { get => player.Score; set => player.Score = value; }
+    public float PlayerTime { get => player.TotalTime; set => player.TotalTime = value; }
+    
     #endregion
 
     #region FUNCTIONS
-
-    [Button("Game init")]
+    /// <summary>
+    /// This function is run at the start of the game/program
+    /// </summary>
     private void _init() {
-        TitleScreen.SetActive(true);
-        GamePlay.SetActive(false);
-        Options.SetActive(false);
-        Pause.SetActive(false);
-        PostGame.SetActive(false);
+        titleScreen.SetActive(true);
+        gamePlay.SetActive(false);
+        options.SetActive(false);
+        pause.SetActive(false);
+        postGame.SetActive(false);
     }
 
     /// <summary>
-    /// A button created to test all of the scenes
+    /// A inspector button used to test game state transitions to the next state
     /// </summary>
-    [Button("nextState")]
+    [ButtonGroup(TestString)]
+    [Button("Next State")]
     private void NextState() {
-        stateNum++;
+        _stateNum++;
 
-        if ( stateNum >= typeof(GameState).GetEnumValues().Length ) {
-            stateNum = 0;
+        if ( _stateNum >= typeof(GameState).GetEnumValues().Length ) {
+            _stateNum = 0;
         }
-//        SoundManager._instance.playSound(SoundManager.soundSelection.playerSelect);
-        transitionToState((GameState)stateNum);
+        TransitionToState((GameState)_stateNum);
     }
 
-    public void transitionToState(GameState state) {
-        SoundManager._instance.playSound(SoundManager.soundSelection.playerSelect);
+    /// <summary>
+    /// A inspector button used to test game state transitions to the previous state
+    /// </summary>
+    [ButtonGroup(TestString)]
+    [Button("Prev State")]
+    private void PrevState() {
+        _stateNum--;
+
+        if ( _stateNum <= 0 ) {
+            _stateNum = typeof(GameState).GetEnumValues().Length;
+        }
+        TransitionToState((GameState)_stateNum);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="state"></param>
+    public void TransitionToState(GameState state) {
+        SoundManager.Instance.PlaySound(SoundManager.SoundSelection.PlayerSelect);
         previousState = currentState;
         currentState = state;
     }
 
-    public void initPlayerData() {
-        _instance.PlayerHealth = 15;
-        _instance.player.score = 0;
-        _instance.player.TotalTime = 0;
+    /// <summary>
+    /// Funtion used to initialise the players base data
+    /// </summary>
+    public void InitPlayerData() {
+        Instance.PlayerHealth = 15;
+        Instance.player.Score = 0;
+        Instance.player.TotalTime = 0;
     }
 
+    /// <summary>
+    /// Function used to clear the screen in preparation for a state change
+    /// </summary>
     private void ClearScreen() {
-        TitleScreen.SetActive(false);
-        GamePlay.SetActive(false);
-        Options.SetActive(false);
-        Pause.SetActive(false);
-        PostGame.SetActive(false);
+        titleScreen.SetActive(false);
+        gamePlay.SetActive(false);
+        options.SetActive(false);
+        pause.SetActive(false);
+        postGame.SetActive(false);
     }
 
     #endregion

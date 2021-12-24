e9aa470a4ad2f1336d19ad483ac3fce9c02ba471
Eddy <young.reaper12@gmail.com>
2021-12-25 00:25:56 +1030

    PC build improvements

--- StroopTest/Assets/Scripts/Screens/GamePlay.cs
+++ StroopTest/Assets/Scripts/Screens/GamePlay.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using Sirenix.OdinInspector.Editor;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using Random = System.Random;
 
 /// <summary>
 /// Resposible for all the script that is run during the gameplay of the game
